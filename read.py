4f3bedf74e1ec68d5740e56caefc3cb2dcd3f799
Henry Xiao <jm928006266@gmail.com>
2021-02-09 20:19:54 +0800

    calc avg len of reviews

--- read.py
+++ read.py
@@ -1,13 +1,17 @@
 data = []
 count = 0
+
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
-			print(len(data))
-print(len(data))
+		    print(len(data))
+print('档案读取完了,总共有',len(data),'笔资料')
 
-print(data[0])
-print('----------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+	
+print('每条留言平均',sum_len/len(data),'字')
+
